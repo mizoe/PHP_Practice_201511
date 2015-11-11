9f4972a23819bc5c56d369e89dc5fe00c7e6c38c
Tomo Mizoe <mz@july.co.jp>
2015-11-11 16:11:51 +0900

    if rensyu1

---
+++ if_rensyu1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+
+</head>
+<body>
+<?php
+$age = 20;
+if($age <= 20){
+    print "成人です";
+}
+
+?>
+</body>
+</html>
